3745220051511917cfba47126ef29f20be868e70
John Kottis <johnkottis@gmail.com>
2020-04-21 18:33:13 +0100

    error handling support

--- unary/square-root/server/server.js
+++ unary/square-root/server/server.js
@@ -7,24 +7,30 @@ var schoolSrv = require('../server/protos/school_grpc_pb');
 
 // implement rpc method
 function sqrt(call, callback) {
-    // implement response entity
-    var answer = new schoolMethods.Student();
+    if (call.request.getQuestion() >= 0) {
+        // implement response entity
+        var answer = new schoolMethods.Student();
 
-    // pass the response value
-    answer.setAnswer(
-        Math.sqrt(call.request.getQuestion())
-    );
-    // send the response
-    callback(null, answer);
+        // pass the response value
+        answer.setAnswer(
+            Math.sqrt(call.request.getQuestion())
+        );
+        // send the response
+        callback(null, answer);
+    } else {
+        return callback({
+            code: grpc.status.INVALID_ARGUMENT,
+            message: "Input is a negative number!!!"
+        })
+    }
 }
-
 
 function main() {
     // create the server
     var server = new grpc.Server();
 
     // add the service
-    server.addService(schoolSrv.TestService, {sqrt:sqrt});
+    server.addService(schoolSrv.TestService, { sqrt: sqrt });
 
     // bind to URL and port
     server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure())
